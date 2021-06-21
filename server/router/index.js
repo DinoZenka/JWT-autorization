d922bea58969532c78202c76062a6f62cdea623b
DinoZenka <maxim.puchko123@gmail.com>
2021-06-22 00:27:16 +0300

    registration validation

--- server/router/index.js
+++ server/router/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router()
 const userController = require('../controllers/userController');
 
+const { body } = require('express-validator'); // for request body validation
 
-
-router.post('/registration', userController.registration);
+router.post('/registration', body('email').isEmail(), body('password').isLength({ min: 3, max: 32 }), userController.registration);
 router.post('/login', userController.login);
 router.post('logout', userController.logout);
 router.get('/activate/:link', userController.activate);
